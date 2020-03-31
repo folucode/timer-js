78bdcd02dc9e445c9248da7e18f3f7170b631a69
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2020-03-31 21:50:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+let circle = document.querySelector('circle');
+
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+console.log(perimeter)
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration;
+
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart(totalDuration) {
+        duration = totalDuration
+    },
+    onTick(timeRemaining) {
+        console.log(perimeter * timeRemaining);
+        circle.setAttribute('stroke-dashoffset', 
+            perimeter * timeRemaining
+        )
+    },
+    onComplete() {
+        console.log('Timer is completed');
+    }
+});
